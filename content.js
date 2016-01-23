714569909e86ce3d53d5f331efcbe53e00a09cd6
Wei Kang <weikangchia@gmail.com>
2016-01-23 23:52:46 +0800

    update random function

--- content.js
+++ content.js
@@ -1,32 +1,38 @@
 //Width and height of video
 var height;
 var width;
+var annoyChance = 50;
 
 function annoy() {
-	var vids = document.getElementsByTagName('video') 
-	// vids is an HTMLCollection
+    var vids = document.getElementsByTagName('video')
+        // vids is an HTMLCollection
 
-	for( var i = 0; i < vids.length; i++ ){ 
-		//Alert the source of the video
-		//alert(vids.item(i).src);
-		if ((vids.item(i).style.height) != 0)
-			height = (vids.item(i).style.height);
-		if ((vids.item(i).style.width) != 0)
-			width = (vids.item(i).style.width);
-		vids.item(i).src = "";
-	}
+    for (var i = 0; i < vids.length; i++) {
+        //Alert the source of the video
+        //alert(vids.item(i).src);
+        if ((vids.item(i).style.height) != 0)
+            height = (vids.item(i).style.height);
+        if ((vids.item(i).style.width) != 0)
+            width = (vids.item(i).style.width);
+        vids.item(i).src = "";
+    }
 
-	try {
-		document.getElementById("player").innerHTML = "";
+    try {
+        document.getElementById("player").innerHTML = "";
 
-		var player = document.getElementById("placeholder-player");
-		player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen style="width:'+width+';height:'+height+';"></iframe>';
-	} catch (err) {}
+        var player = document.getElementById("placeholder-player");
+        player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen style="width:' + width + ';height:' + height + ';"></iframe>';
+    } catch (err) {}
 }
 
 var title = document.getElementsByTagName('title');
-for( var i = 0; i < title.length; i++ ){ 
-	title.item(i).addEventListener("DOMSubtreeModified", function(evt) {
-		annoy();
-	}, false);
+for (var i = 0; i < title.length; i++) {
+    title.item(i).addEventListener("DOMSubtreeModified", function (evt) {
+        var randVal = Math.floor((Math.random() * 100) + 1);
+        if (randVal < annoyChance) {
+            annoy();
+        } else {
+            location.reload();
+        }
+    }, false);
 }
