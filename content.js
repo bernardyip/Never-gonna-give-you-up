3816b177847cbfe39c862c384b0f0104ab1417eb
bernardyip <bernard_yip40@hotmail.com>
2016-01-23 17:55:49 +0800

    Fixed old video's sound still playing
    now video is totally removed

--- content.js
+++ content.js
@@ -1,19 +1,31 @@
-var vids = document.getElementsByTagName('video') 
-// vids is an HTMLCollection
+function annoy() {
+	var vids = document.getElementsByTagName('video') 
+	// vids is an HTMLCollection
 
-//Width and height of video
-var height;
-var width;
+	//Width and height of video
+	var height;
+	var width;
 
-for( var i = 0; i < vids.length; i++ ){ 
-	//Alert the source of the video
-	//alert(vids.item(i).src);
-	height = (vids.item(i).style.height);
-	width = (vids.item(i).style.width);
-	//ID player
+	for( var i = 0; i < vids.length; i++ ){ 
+		//Alert the source of the video
+		//alert(vids.item(i).src);
+		height = (vids.item(i).style.height);
+		width = (vids.item(i).style.width);
+		vids.item(i).src = "";
+		//ID player
+	}
+
+	document.getElementById("player").innerHTML = "";
+
+	var player = document.getElementById("placeholder-player");
+	player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen autoplay=1 style="width:'+width+';height:'+height+';"></iframe>';
 }
 
-document.getElementById("player").innerHTML = "";
+/*
+chrome.tabs.onUpdated.addListener(function () {
+	alert("Hello World");
+})
+*/
 
-var player = document.getElementById("placeholder-player");
-player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen autoplay=1 style="width:'+width+';height:'+height+';"></iframe>';
+//Run functions
+annoy();
