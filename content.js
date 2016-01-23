cb5292a0522c45518fb97cf274f433ae0073a7a4
bernardyip <bernard_yip40@hotmail.com>
2016-01-23 16:14:12 +0800

    Detect video source

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
-if ( yourVideoElement.readyState === 4 ) {
-    alert("Hello from your Chrome extension!")
+var vids = document.getElementsByTagName('video') 
+// vids is an HTMLCollection
+for( var i = 0; i < vids.length; i++ ){ 
+	//Alert the source of the video
+	alert(vids.item(i).src);
+    //vids.item(i).src = "blob:https%3A//www.youtube.com/8faa2e29-6835-4f3d-8a4f-373dec3e7209";
 }
