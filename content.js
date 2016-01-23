42f82a1aa13fba5acb81300fee08b9a891e341ed
bernardyip <bernard_yip40@hotmail.com>
2016-01-23 20:34:58 +0800

    Remove lousy comments

--- content.js
+++ content.js
@@ -14,13 +14,12 @@ function annoy() {
 		if ((vids.item(i).style.width)!=0)
 			width = (vids.item(i).style.width);
 		vids.item(i).src = "";
-		//ID player
 	}
 
 	document.getElementById("player").innerHTML = "";
 
 	var player = document.getElementById("placeholder-player");
-	player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen autoplay=1 style="width:'+width+';height:'+height+';"></iframe>';
+	player.innerHTML = '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen style="width:'+width+';height:'+height+';"></iframe>';
 }
 
 var title = document.getElementsByTagName('title');
